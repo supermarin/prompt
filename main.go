89968f22821a698e67109777a329fff76ada4860
Marin Usalj <marin2211@gmail.com>
2018-08-02 20:44:14 -0700

    works good, but slow af

--- main.go
+++ main.go
@@ -12,6 +12,16 @@ import (
 	"github.com/libgit2/git2go"
 )
 
+const format = "\x1b[%dm"
+const blue = 34
+const red = 31
+const yellow = 33 // TODO: replace with magenta
+const reset = "\x1b[0m"
+
+func colored(s string, color int) string {
+	return fmt.Sprintf(format+s+reset, color)
+}
+
 func main() {
 	// prompt storage
 	buf := new(bytes.Buffer)
@@ -20,13 +30,15 @@ func main() {
 	fatalIfError(err)
 
 	usr, err := user.Current()
+	var wdout string
 	if err != nil {
-		buf.WriteString(wd)
+		wdout = wd
 	} else {
 		r, err := filepath.Rel(usr.HomeDir, wd)
 		fatalIfError(err)
-		buf.WriteString("~/" + r)
+		wdout = "~/" + r
 	}
+	buf.WriteString(colored(wdout, blue))
 
 	var repo *git.Repository
 	if found, err := git.Discover(wd, false, nil); err == nil {
@@ -48,24 +60,29 @@ func main() {
 	detached, err := repo.IsHeadDetached()
 	fatalIfError(err)
 
+	var refout string
 	if detached {
 		name, err := head.Peel(git.ObjectAny)
 		fatalIfError(err)
 		headName, err := name.ShortId()
 		fatalIfError(err)
-		buf.WriteString(" " + headName)
+		refout = headName
 	} else {
 		// Not detached, it should be in a branch
 		branch, err := head.Branch().Name()
 		fatalIfError(err)
-		buf.WriteString(" ⌥ " + branch)
+		refout = "⌥ " + branch
 	}
 
-	// dirty
-	_, err = exec.Command("git", "status", "--porcelain").Output()
-	if err == nil {
-		buf.WriteString(" M")
+	out, err := exec.Command("git", "status", "--porcelain").Output()
+	fatalIfError(err)
+	if len(out) > 0 {
+		// dirty
+		refout = colored(refout, red)
+	} else if detached {
+		refout = colored(refout, yellow)
 	}
+	buf.WriteString(" " + refout)
 
 	// stashes
 	var count int
@@ -79,7 +96,8 @@ func main() {
 	}
 	// final Exit
 	buf.WriteString(" $ ")
-	buf.WriteTo(os.Stdout)
+	// buf.WriteTo(os.Stdout)
+	fmt.Print(buf.String())
 }
 
 func fatalIfError(err error) {
